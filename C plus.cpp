7a072688b064bddd1bff1586ad5efb74f703e74c
Mudit Bansal <muditbansal95@gmail.com>
2016-05-20 22:43:43 +0530

    Update C plus.cpp
    Checking the random function tip given in Google I/O about compressing data.

--- C plus.cpp
+++ C plus.cpp
@@ -1,6 +1,23 @@
 #include<iostream.h>
+#include<math.h>
+#include<conio.h>
 
 void main()
 {
-cout<<"Hello World from c++";
+int a,b,c;
+a=0;
+b=1;
+c=3;
+ for(int i = 0; i<c; i++)
+  {
+    cout<<rand(10);
+  }
+ for(i = 0; i<c; i++)
+ {
+   cout<<rand(10);
+ }
+ for(i=0; i<b; i++)
+ {
+   cout<<rand(9);
+ }
 }
